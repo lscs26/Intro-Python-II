1d758a698d2f6f295f88266d84a47dd8b71bcc29
kenna <kennaalastair@gmail.com>
2020-02-12 19:59:53 -0800

    printing out player's name and current room

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,10 +39,12 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player('Kenna', currentRoom=room['outside'])
 
 # Write a loop that:
 #
 # * Prints the current room name
+print(player)
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
